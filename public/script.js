29909d6558c375835e5201f81bc57979c30f6563
Simon <simon160@gmail.com>
2016-02-27 14:23:04 -0500

    nothing works

--- public/script.js
+++ public/script.js
@@ -1,11 +1,39 @@
-// script.js
+// create the module and name it scotchApp
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-    // create the module and name it scotchApp
-    var scotchApp = angular.module('scotchApp', []);
+// configure our routes
+scotchApp.config(function($routeProvider) {
+  $routeProvider
 
-    // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
+    // route for the home page
+    .when('/', {
+      templateUrl : 'pages/home.html',
+      controller  : 'mainController'
+    })
 
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
+    // route for the about page
+    .when('/about', {
+      templateUrl : 'pages/about.html',
+      controller  : 'aboutController'
+    })
+
+    // route for the contact page
+    .when('/contact', {
+      templateUrl : 'pages/contact.html',
+      controller  : 'contactController'
     });
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+  // create a message to display in our view
+  $scope.message = 'Everyone come and see how good I look!';
+});
+
+scotchApp.controller('aboutController', function($scope) {
+  $scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function($scope) {
+  $scope.message = 'Contact us! JK. This is just a demo.';
+});
